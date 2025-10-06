7bde1f2b1abdf02688fba515880aaff5b942d581
ShreyJoshi74 <shreyjoshi199@gmail.com>
2025-10-06 11:02:08 +0530

    Time: 16 ms (97.48%) | Memory: 18.4 MB (72.35%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.py
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -1,28 +1,28 @@
+import heapq
+
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        queue = deque()
         n = len(grid)
-        vis = [[False for _ in range(n)] for _ in range(n)] 
-        time = [[2 ** 31 for _ in range(n)] for _ in range(n)]
-        direction = [[1,0],[-1,0],[0,1],[0,-1]]
-        queue.append([0,0,grid[0][0]])
-        time[0][0] = grid[0][0]
-        while queue:
-            current = queue.popleft()
-            currentTime = current[2]
-            i = current[0]
-            j = current[1]
+        vis = [[False for _ in range(n)] for _ in range(n)]
+        direction = [(1,0), (-1,0), (0,1), (0,-1)]
 
-            for dirr in direction:
-                ni = i + dirr[0]
-                nj = j + dirr[1]
-                if(ni >= 0 and ni < n and nj >= 0 and nj < n and time[ni][nj] > max(currentTime , grid[ni][nj])):
-                    time[ni][nj] = max(currentTime,grid[ni][nj])
-                    queue.append([ni,nj,time[ni][nj]])
-                    
-        return time[n-1][n-1]
+        # Min-heap: (time, i, j)
+        pq = [(grid[0][0], 0, 0)]  
+        vis[0][0] = True
+        time = 0
 
-
-
-
-
+        while pq:
+            t, i, j = heapq.heappop(pq)
+            time = max(time, t)
+            
+            # reached destination
+            if i == n-1 and j == n-1:
+                return time
+            
+            for di, dj in direction:
+                ni, nj = i + di, j + dj
+                if 0 <= ni < n and 0 <= nj < n and not vis[ni][nj]:
+                    vis[ni][nj] = True
+                    heapq.heappush(pq, (grid[ni][nj], ni, nj))
+        
+        return time
